460c7fb7a0f5d3c6bf8fbe0f132262f17ed00237
SR <stanislav.roudavski@cantab.net>
2012-09-12 00:14:14 +1000

    Renamed folder

---
+++ Stigmergy2D 01/src/stig2D/Mover.java
@@ -0,0 +1,124 @@
+package stig2D;
+
+import processing.core.PVector;
+
+public class Mover {
+
+	Stig2D p5;
+	PVector location;
+	PVector velocity;
+	PVector acceleration;
+	PVector lastPos;
+	float mass;
+	int c;
+
+	Mover(Stig2D p5, PVector location, PVector velocity, float mass){
+
+		this.p5 = p5;
+		this.mass = mass;
+		this.location = location;
+		this.velocity = velocity;
+		acceleration = new PVector(0, 0);
+		lastPos = location;
+		c = 0;
+	}
+
+	void run(){
+
+		update();
+		checkEdges();
+		checkWorld();
+		modWorld();
+		// display();
+	}
+
+	//move the object
+	void update() {
+		velocity.add(acceleration);
+		velocity.limit(p5.maxSpeed);
+		location.add(velocity);
+		acceleration.mult(0);
+		if (c % p5.searchRad ==0) lastPos = new PVector(location.x,location.y,location.z); 
+	}
+
+	//draw the object
+	void display() {
+		p5.ellipse(location.x, location.y, 2, 2);
+	}
+
+	void applyForce(PVector force) {
+		PVector f = PVector.div(force,mass);
+		acceleration.add(f);
+	}
+
+	//check the edge of the screen
+	void checkEdges() {
+
+		if (location.x > p5.width) {
+			location.x = 0;
+		} 
+		else if (location.x < 0) {
+			location.x = p5.width;
+		}
+
+		if (location.y > p5.height) {
+			location.y = 0;
+		} 
+		else if (location.y < 0) {
+			location.y = p5.height;
+		}
+	}
+
+	//check the environment around the object
+	void checkWorld(){
+		PVector toBest = new PVector(); //create a temp vector to keep track of the direction of the best condition
+		float maxV = 0;
+
+		//loop through pixels
+		for (int i = -p5.searchRad; i <= p5.searchRad; i++){
+			for (int j = -p5.searchRad; j <= p5.searchRad; j++){
+				if(!(i == 0 && j == 0)){
+					//checks for edges
+					int x = Stig2D.floor(location.x)+i;
+					x = Stig2D.constrain(x, 0, p5.width-1);
+					int y = Stig2D.floor(location.y)+j;
+					y = Stig2D.constrain(y, 0, p5.height-1);
+
+					//check to see if this is the smallest current value
+					//scale by the distance to the value
+					float v = p5.world.getAt(x, y);
+					PVector toV = new PVector(i, j);
+
+					//limit the angle of vision
+					if(PVector.angleBetween(toV,velocity)<Stig2D.PI/2){
+
+						//check to see if the current value is larger than 
+						//the current best
+						if((v) > maxV){
+
+							//reset all our variables that keep track of the best option
+							float d = toV.mag();
+							toV.mult(1/d);
+
+							toBest = toV;
+							maxV = v;
+						}
+					}
+				}
+			}
+		}
+
+		//only effect if finding something above a tolerance
+		if(maxV>20){
+			applyForce(toBest);
+		}
+	}
+
+	void modWorld(){
+		//checks for edges
+		if(lastPos.x < p5.width-1 && lastPos.y < p5.height-1 && lastPos.x > 0 && lastPos.y > 0) p5.world.modAt(Stig2D.floor(lastPos.x), Stig2D.floor(lastPos.y), 50);
+	}
+
+}
+
+
---
+++ Stigmergy2D 01/src/stig2D/World.java
@@ -0,0 +1,80 @@
+package stig2D;
+
+import processing.core.PImage;
+
+public class World {
+
+	Stig2D p5;
+	PImage loadedImage; // image to store
+	float[][] vals;
+	int w, h;
+
+	World(Stig2D p5, String toImg){
+
+		this.p5 = p5;
+		loadedImage = p5.loadImage(toImg); //load the source image
+		loadedImage.resize(p5.width, p5.height);
+		loadedImage.loadPixels(); //load the pixel array
+		w = loadedImage.width;
+		h = loadedImage.height;
+		vals = new float[w][h];
+
+		loadVals();
+		//loadBlank(); //comment out to load the image
+	}
+
+	void loadVals(){
+		//fill up the brightness values array
+		for (int i = 0; i < w; i++){
+			for (int j = 0; j < h; j++){
+				vals[i][j] = p5.brightness(loadedImage.pixels[(j*w)+i]); 
+			}
+		} 
+	}
+
+	void loadBlank(){
+		for (int i = 0;i<w;i++){
+			for (int j = 0;j<h;j++){
+				vals[i][j]=0; 
+			}
+		} 
+	}
+
+	//get a value from the image 
+	float getAt(int x, int y){
+		return vals[x][y];
+	}
+
+	//fade the image
+	void fade(){
+		for (int i = 0; i < w; i++){
+			for (int j = 0; j < h; j++){
+				if(vals[i][j] > p5.fadeSpeed) vals[i][j] = vals[i][j] - p5.fadeSpeed; 
+			}
+		}
+	}
+
+
+	//write a new value to the image
+	void setAt(int x, int y, float v){
+		v = Stig2D.constrain(v, 0, 255);
+		vals[x][y] = v;
+	}
+
+	void modAt(int x, int y, float v){
+		float mod = Stig2D.constrain(vals[x][y] + v, 0, 255);
+		vals[x][y] = mod;
+	}
+
+	void drawImage(){
+		p5.loadPixels();
+		for (int i = 0; i < w; i++){
+			for (int j = 0; j < h; j++){
+				float b= Stig2D.constrain(vals[i][j], 0, 255);
+				p5.pixels[(j*w)+i] = p5.color(b);
+			}
+		}
+		p5.updatePixels();
+	}
+
+}
